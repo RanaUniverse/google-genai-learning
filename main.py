ce7a29cde7096f559e46f574d99ad5fed4abc370
RanaUniverse <RanaUniverse321@gmail.com>
2025-07-12 17:40:47 +0530

    differentiate the ai response in a fun so that i can call thsi fun in other place easily

--- main.py
+++ main.py
@@ -1,43 +1,47 @@
 import os
 
-
 from dotenv import load_dotenv
 
 from google import genai
-from google.genai import types
-
 
 load_dotenv()
 
 private_api_key = os.environ.get("GEMINI_API_KEY")
-
-
 client = genai.Client(api_key=private_api_key)
-
 MODEL_NAME = "gemini-2.5-flash"
 
-prompt = "prove the pythagorus theorem text by text"
 
-response = client.models.generate_content(  # type: ignore
-    model=MODEL_NAME,
-    contents=prompt,
-    config=types.GenerateContentConfig(
-        thinking_config=types.ThinkingConfig(
-            thinking_budget=1024,
-            include_thoughts=True,
-        )
-    ),
-)
+# prompt = "state pythogorus themorem in math in 15 word"
+# response = client.models.generate_content(  # type: ignore
+#     model=MODEL_NAME,
+#     contents=prompt,
+# )
+# print(response.text)
 
 
-for part in response.candidates[0].content.parts:
-    if not part.text:
-        continue
-    if part.thought:
-        print("Thought summary:")
-        print(part.text)
-        print()
-    else:
-        print("Answer:")
-        print(part.text)
-        print()
+def answer_question_from_ai(question: str) -> str | None:
+    """
+    I will call this function in need time
+    which will allow a output a which i will use later
+    """
+
+    response = client.models.generate_content(  # type: ignore
+        model=MODEL_NAME,
+        contents=question,
+    )
+
+    if not response.text:
+        return None
+
+    answer = f"{response.text}" f"\n\n\n" f"-RanaUniverse"
+
+    return answer
+
+
+if __name__ == "__main__":
+    
+    question = "What is ai say in 10 word?"
+
+    ai_response = answer_question_from_ai(question)
+    print(ai_response)
+    print("Thanks")
